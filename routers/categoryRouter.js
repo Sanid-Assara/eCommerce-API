3addb0a7fa56d43021fd91de12a40ba0b2cc95ab
Sanid-Assara <sanid.assara@gmail.com>
2024-07-30 22:05:12 +0200

    Create router for Categories

---
+++ routers/categoryRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/categories.js";
+
+const categoriesRouter = express.Router();
+
+categoriesRouter.get("/", getCategories);
+categoriesRouter.category("/", createCategory);
+categoriesRouter.get("/:id", getCategory);
+categoriesRouter.put("/:id", updateCategory);
+categoriesRouter.delete("/:id", deleteCategory);
+
+export default categoriesRouter;
