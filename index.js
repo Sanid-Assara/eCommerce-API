c6a9e141020e20f8fcbab1605df9ab931df09988
Sanid-Assara <sanid.assara@gmail.com>
2024-07-30 22:20:33 +0200

    Typo fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import categoriesRouter from "./routers/categoriesRouter.js";
 import ordersRouter from "./routers/ordersRouter.js";
 import productsRouter from "./routers/productsRouter.js";
-import postsRouter from "./routers/postsRouter.js";
+import usersRouter from "./routers/usersRouter.js";
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/api/categories", categoriesRouter);
 app.use("/api/orders", ordersRouter);
 app.use("/api/products", productsRouter);
-app.use("/api/users", postsRouter);
+app.use("/api/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(
