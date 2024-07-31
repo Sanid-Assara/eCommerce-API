497ff394167210ec7b9d85859684c7cfae5af58b
Michal <michalochecki00@gmail.com>
2024-07-31 12:14:55 +0200

    added product router

---
+++ routers/productRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/products.js";
+
+const productsRouter = express.Router();
+
+productsRouter.get("/", getProducts);
+productsRouter.post("/", createProduct);
+productsRouter.get("/:id", getProduct);
+productsRouter.put("/:id", updateProduct);
+productsRouter.delete("/:id", deleteProduct);
+
+export default productsRouter;
