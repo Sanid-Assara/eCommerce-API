3684f0b833a7a411cbc1be1af6d6cfb7fd5c6a0b
Michal <michalochecki00@gmail.com>
2024-07-31 14:59:14 +0200

    other parts have been added

--- db/associations.js
+++ db/associations.js
@@ -1,6 +1,9 @@
 import Category from './Category.js';
 import Product from './Product.js';
 import User from './User.js';
+import Order from './Order.js';
+import OrderProduct from './OrderProduct.js';
+
 
 export const setupAssociations = () => {
   Product.belongsTo(Category, {
@@ -13,14 +16,28 @@ export const setupAssociations = () => {
     as: 'products',
   });
 
-  Product.belongsTo(User, {
+  Order.belongsTo(User, {
     foreignKey: 'userId',
     as: 'user',
   });
 
-  User.hasMany(Product, {
+  User.hasMany(Order, {
     foreignKey: 'userId',
+    as: 'orders',
+  });
+
+  Order.belongsToMany(Product, {
+    through: OrderProduct,
+    foreignKey: 'orderId',
+    otherKey: 'productId',
     as: 'products',
+  });
+
+  Product.belongsToMany(Order, {
+    through: OrderProduct,
+    foreignKey: 'productId',
+    otherKey: 'orderId',
+    as: 'orders',
   });
 
   Category.belongsTo(User, {
@@ -35,9 +52,11 @@ export const setupAssociations = () => {
 };
 
 const syncModels = async () => {
+  await User.sync();
   await Category.sync();
   await Product.sync();
-  await User.sync();
+  await Order.sync();
+  await OrderProduct.sync();
 };
 
 export { syncModels };
