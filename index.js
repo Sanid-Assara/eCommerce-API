d903384f4bbf97725ac170ca947beb451b28f5ab
Michal <michalochecki00@gmail.com>
2024-08-01 22:39:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import categoriesRouter from "./routers/categoryRouter.js";
 import ordersRouter from "./routers/orderRouter.js";
-// import productsRouter from "./routers/productRouter.js";
-// import usersRouter from "./routers/userRouter.js";
+import productsRouter from "./routers/productRouter.js";
+import usersRouter from "./routers/userRouter.js";
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -12,8 +12,8 @@ app.use(express.json());
 
 app.use("/api/categories", categoriesRouter);
 app.use("/api/orders", ordersRouter);
-// app.use("/api/products", productsRouter);
-// app.use("/api/users", usersRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(
